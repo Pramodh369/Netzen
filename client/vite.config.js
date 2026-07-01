b34969fe2e0f25a49a0c1e5af7ec8d33e3799cf9
Pramodh369 <246414613+Pramodh369@users.noreply.github.com>
2026-07-01 13:21:52 +0530

    feat: dockerize application with Docker Compose

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,11 +9,13 @@ export default defineConfig({
   ],
 
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
+  host: "0.0.0.0",
+  port: 5173,
+  proxy: {
+    "/api": {
+      target: "http://localhost:5000",
+      changeOrigin: true,
     },
   },
+},
 }); 
